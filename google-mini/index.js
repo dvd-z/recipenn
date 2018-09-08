c30565212baa0b74a77a2a8e40a7d1ba37c4d5cc
Matthew Yuen <un.matthew@gmail.com>
2018-09-08 19:12:25 -0400

    Added basic intent function to test

---
+++ google-mini/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const {dialogflow} = require('actions-on-google');
+const functions = require('firebase-functions');
+const app = dialogflow({
+	debug: true
+});
+
+app.intent('add ingredient', (conv, {unitinformationname}) => {
+	conv.close("Test");
+})
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
