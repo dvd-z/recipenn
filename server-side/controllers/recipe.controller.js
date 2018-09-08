55159fbcf679babf0010ac5f1b85e2f731a4bd19
Vishvam Mazumdar <30667536+v7770x@users.noreply.github.com>
2018-09-08 19:40:14 -0400

    updated user model with pwd

--- server-side/controllers/recipe.controller.js
+++ server-side/controllers/recipe.controller.js
@@ -1,10 +1,12 @@
 const Recipe = require('../models/recipe.model');
+const Login = require('../controllers/login.controller');
 
 exports.test = function(req, res){
     res.send('Greetings from the Test controller!');
 }
 
 exports.create_recipe= function(req, res){
+
     let recipe = new Recipe(
         {
             name: req.body.name,
--- server-side/models/user.model.js
+++ server-side/models/user.model.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 let UserSchema = new Schema({
     name: {type:String, required:true},
     owned_ingredients: {type: Array, required:true},
-    favorite_recipes:{type: Array, required:true}
+    favorite_recipes:{type: Array, required:true},
+    password: {type:String, required:true}
 });
 
 module.exports = mongoose.model("User", UserSchema);
