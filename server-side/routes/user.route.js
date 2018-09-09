8e15e055c1e9a43e6f4b8ec7d18063cef531e6d4
Vishvam Mazumdar <30667536+v7770x@users.noreply.github.com>
2018-09-09 02:57:26 -0400

    loaded dataset to database in correct format

--- server-side/routes/user.route.js
+++ server-side/routes/user.route.js
@@ -5,7 +5,7 @@ const user_controller = require('../controllers/user.controller');
 
 router.post("/create_user", user_controller.create_user );
 router.post("/delete_user", user_controller.delete_user );
-router.post("/add_ingredient", user_controller.add_ingredient);
+router.post("/add_ingredients", user_controller.add_ingredients);
 router.post("/remove_ingredient", user_controller.remove_ingredient);
 router.post("/remove_all_ingredients", user_controller.remove_all_ingredients);
 
