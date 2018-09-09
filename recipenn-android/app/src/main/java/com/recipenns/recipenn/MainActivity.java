7cd4b4a62aab7487a3ddbfc6de3c04a5c7544b7c
Jason Chu <jjchu@uwaterloo.ca>
2018-09-09 02:39:08 -0400

    State before attempting to get recycler to work

--- recipenn-android/app/src/main/java/com/recipenns/recipenn/MainActivity.java
+++ recipenn-android/app/src/main/java/com/recipenns/recipenn/MainActivity.java
@@ -1,13 +1,25 @@
 package com.recipenns.recipenn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button ingredientButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ingredientButton = findViewById(R.id.ingredientButton);
+
+        ingredientButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
     }
 }
--- recipenn-android/app/src/main/java/com/recipenns/recipenn/RecipeActivity.java
+++ recipenn-android/app/src/main/java/com/recipenns/recipenn/RecipeActivity.java
@@ -2,23 +2,11 @@ package com.recipenns.recipenn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 
 public class RecipeActivity extends AppCompatActivity {
 
-    private RecyclerView recycler;
-    private RecyclerView.Adapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        recycler = findViewById(R.id.recipeRecycler);
-        recycler.setHasFixedSize(true);
-
-        layoutManager = new LinearLayoutManager(this);
-        recycler.setLayoutManager(layoutManager);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
     }
