b718964b4794f8ee86517ffc75acc468a1686465
Jason Chu <jjchu@uwaterloo.ca>
2018-09-09 02:48:14 -0400

    Pre-adapter

--- recipenn-android/app/src/main/java/com/recipenns/recipenn/RecipeActivity.java
+++ recipenn-android/app/src/main/java/com/recipenns/recipenn/RecipeActivity.java
@@ -2,12 +2,23 @@ package com.recipenns.recipenn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class RecipeActivity extends AppCompatActivity {
+public class RecipeActivity extends AppCompatActivity {x
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+        recyclerView = findViewById(R.id.recipeRecycler);
+
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
